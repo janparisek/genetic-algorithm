3afdb4de1fef374580e085cd4b21c9c1fe4454a6
Jan Parisek <Jan.Parisek@stud.h-da.de>
2022-01-17 00:02:37 +0100

    Add random selection

--- src/main/java/de/h_da/fbi/ga/mo12/parisek/genetics/Generation.java
+++ src/main/java/de/h_da/fbi/ga/mo12/parisek/genetics/Generation.java
@@ -2,11 +2,13 @@ package de.h_da.fbi.ga.mo12.parisek.genetics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Generation {
     private List<Protein> candidates = new ArrayList<>();
     private Integer number = 0;
     private Double averageFitness = 0d;
+    private Double totalFitness = 0d;
     private Protein bestCandidate;
     private final static Integer GENERATION_SIZE = 100;
 
@@ -26,12 +28,12 @@ public class Generation {
         Protein protein = new Protein(sequence);
         bestCandidate = protein;
         candidates.add(protein);
-        averageFitness += protein.getFitness();
+        totalFitness += protein.getFitness();
 
         for(int i = 1; i < GENERATION_SIZE; ++i) {
             protein = new Protein(sequence);
             candidates.add(protein);
-            averageFitness += protein.getFitness();
+            totalFitness += protein.getFitness();
 
             if(protein.getFitness() > bestCandidate.getFitness()) {
                 bestCandidate = protein;
@@ -39,13 +41,23 @@ public class Generation {
 
         }
 
-        averageFitness = averageFitness / (double) GENERATION_SIZE;
+        averageFitness = totalFitness / (double) GENERATION_SIZE;
 
     }
 
     public Generation generateNext() {
-        //TODO: Genetischer Algorithmus hier
+        List<Protein> nextCandidates = new ArrayList<>();
+
+        nextCandidates.add()
+
+
         return null;
+    }
+
+    private Double getRandomDouble(Double rangeMin, Double rangeMax) {
+        Random r = new Random();
+        Double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        return randomValue;
     }
 
 }
